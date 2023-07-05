7279b7f5f47ded43df8e02ab9cf9622f00c59084
Wen-Jie Tseng <wen-jie.tseng@telecom-paris.fr>
2023-07-05 18:00:57 +0200

    pointing error and procedure done

--- Assets/Scripts/CalculatePointingError.cs
+++ Assets/Scripts/CalculatePointingError.cs
@@ -1,51 +1,140 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Oculus.Interaction;
+using TMPro;
 
 public class CalculatePointingError : MonoBehaviour
 {
-    
-    public bool isStart;
+    public bool isDebug;
+    public OVRInput.Controller _leftController;
+    public OVRInput.Controller _rightController;
     public List<GameObject> targetList;
-    public GameObject _leftController;
-    public GameObject hmd;
-    public Vector3 head2Object;
-    public Vector3 pointingVec;
+    public TMP_Text taskInstruction;
+    public bool isStartTask;
+    private int taskNum;
+    private string env;
+    [SerializeField] private float t0;
+    [SerializeField] private float t1; // the time when participants are done accessing VR/RW SMM
+    [SerializeField] private float t2; // the time when participants confirm the assigned object direction
+    private bool taskIsRunning;
+    private bool pointingStart;
+    private bool isPause;
+
+    [Header("Calculating Pointing Angular Error")]
+    public Vector3 controllerToObjectVec;
+    public Vector3 pointingVector;
+    public GameObject rayPointer;
+    public float angularError;
+
     
     void Start()
     {
+        // Shuffle the order of pointing task
+        Helpers.Shuffle(targetList);
+        taskNum = 0;    
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isStart)
+        if (taskNum < targetList.Count)
         {
-            Helpers.Shuffle(targetList);
-            for (int i = 0; i < targetList.Count; i++)
+            if (isStartTask)
             {
-                print(targetList[i].name);
+                isStartTask = false; // a task is initaited
+                taskIsRunning = true;
+                t0 = 0.0f;
+                t1 = -1.0f;
+                t2 = -1.0f;
+
+                if (targetList[taskNum].tag == "VirtualObjects") env = "VR";
+                else env = "Real-World";
+
+                // ask participants to access VR/RW SMM.
+                taskInstruction.text = "Please think about the " + env + " environment and\n" + "push the trigger when you are ready";
             }
-            isStart = false;
+            else
+            {
+                if (taskIsRunning)
+                {
+                    t0 += Time.deltaTime;
+                    
+                    if (t1 <= 0.0f)
+                    {
+                        if (OVRInput.GetDown(OVRInput.Button.One, _rightController))
+                        {
+                            // record t1 when SMM is ready
+                            t1 = t0;
+                            taskInstruction.text = "Please point toward the center of " + targetList[taskNum].name + " and\n" + "push the trigger to confirm";
+                            pointingStart = true;
+                        }
+                    }
+                    else
+                    {
+                        if (pointingStart)
+                        {
+                            ComputeAngularError();
+                            if (OVRInput.GetDown(OVRInput.Button.One, _rightController))
+                            {
+                                // record t2 when pointing toward the target and confirm
+                                t2 = t0;
+                                // calculate pointing error                                
+                                Debug.Log(angularError); // record data
+                                pointingStart = false;
+                                StartCoroutine(ShortBreakForPointingTask(3.0f));
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    if (isPause)
+                    {
+                        t0 -= Time.deltaTime;
+                        taskInstruction.text = t0.ToString();
+                    }
+                }
 
-
-            
+            }
         }
 
-        // write a helper fucntion, double check it is correct.
-        head2Object = (targetList[0].transform.position - hmd.transform.position);
-        head2Object = Vector3.ProjectOnPlane(head2Object, Vector3.up);
-        Debug.DrawRay(hmd.transform.position, head2Object, Color.red, 0.1f);
 
-        pointingVec = Vector3.ProjectOnPlane(_leftController.transform.forward, Vector3.up);
-        Debug.DrawRay(_leftController.transform.position, pointingVec, Color.green, 0.1f);
+        if (isDebug)
+        {
+        //     Debug.Log(OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, _leftController) + "," + 
+        //             OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, _rightController)); 
+        }
 
-        Debug.Log(Vector3.Angle(head2Object, pointingVec));
-        
+              
     }
     
+    public IEnumerator ShortBreakForPointingTask(float duration)
+    {
+        taskIsRunning = false;
+        isPause = true;
+        t0 = 3.0f;
+        taskNum += 1;
+        yield return new WaitForSeconds(duration);
+        isPause = false;
+        isStartTask = true;
+    }
+
+    void ComputeAngularError()
+    {
+        controllerToObjectVec = (targetList[taskNum].transform.position - OVRInput.GetLocalControllerPosition(_rightController));
+        Vector3 a = Vector3.ProjectOnPlane(controllerToObjectVec, Vector3.up);
+        Vector3 b = Vector3.ProjectOnPlane(rayPointer.transform.forward, Vector3.up);
+        angularError = Vector3.Angle(a, b);
+        if (isDebug)
+        {
+            Debug.DrawRay(OVRInput.GetLocalControllerPosition(_rightController), a, Color.red, 0.1f);
+            Debug.DrawRay(OVRInput.GetLocalControllerPosition(_rightController), b*10, Color.green, 0.1f);
+            // the ray is under ControllerRayInteractor>Visuals>ControllerRay>Cube
+            Debug.DrawRay(OVRInput.GetLocalControllerPosition(_rightController), b*10, Color.blue, 0.1f);            
+        }
+    }
+
 }
 
 
@@ -64,4 +153,7 @@ public static class Helpers
             list[r] = tmp;
         }
     }
+
+
+
 }
